c58984c052a1bfa3eeb139097d73da24cb68e44e
Debraj Ray <debraj1990@gmail.com>
2019-03-13 07:05:24 +0530

    1) styling for signout button in "ProfileSection.scss" 2) componentDidMount authcheck in "ProfileSection/index.js" & "templates/Pages/Home.js"

--- capstone/client/src/components/templates/Pages/Home.js
+++ capstone/client/src/components/templates/Pages/Home.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import firebase from 'firebase';
 import CategoryList from '../../organisms/CategoryList';
 
 
@@ -9,6 +10,26 @@ const metaData = {
 };
 
 class Home extends Component {
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    //we need to try implementing this logic somewhere in routing to be applied on all pages except login as reverse needs to be applied there
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => {console.log(user);
+                  if(!user){
+                      this.props.history.push(`/login`); //user logged out
+                } else {
+                  // Logic to check for record in mongoDB and save a new record if not found
+                  // this.currentUserEmail = JSON.parse(sessionStorage.getItem('userAuthInfo')).user.email;
+                }
+              }
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
   render() {
     return (
       <div>
