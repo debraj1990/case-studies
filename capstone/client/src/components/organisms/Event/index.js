9d3f082cf316bd1b2afb7f4c110edb7a27bc2797
Sachin Manchanda <sacmanch@publicisgroupe.net>
2019-03-18 08:47:30 +0530

    Event  UI changed

--- capstone/client/src/components/organisms/Event/index.js
+++ capstone/client/src/components/organisms/Event/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default ({ event, onDelete }) => {
     return (
-        <div className="col-8 col-sm-12 col-md-12">
+        <div className="col-4 col-sm-4 col-md-4 float-left">
             <div className="alert alert-info">
                 <span className="badge">{event.type}</span> &mdash; <span>{new Date(event.date).toDateString()}</span>
                 <i onClick={e => onDelete(event.id)} className="fa fa-times-circle float-right"></i>
