73820d93255a2f56fbc537f312f0d73b74db0411
XT-training <>
2019-03-18 14:40:58 +0530

    no message

--- capstone/client/src/components/molecules/WishlistProduct/index.js
+++ capstone/client/src/components/molecules/WishlistProduct/index.js
@@ -8,25 +8,29 @@ class WishlistProduct extends Component {
 
   render() {
     let { product } = this.props;
-    let tgtUrl = product ? `/product/${product.id}` : '/';
-    let variant = product ? product.variants[0] : {};
-    let imgUrl = variant.images[0].path.split('/').pop();
-    let prodObj = {
-      product: product.id,
-      sku: variant.sku
-    }
-    return (
-      <div className="col-6 col-sm-3 wishlist-prod">
-        <div className="wishlist-prod-container">
-          <div className="float-right"><AddToWishlist wishlistObj={prodObj} /></div>
-          <Link to={tgtUrl}>
-            <img src={images[imgUrl]} alt={product.name} className="img-responsive" style={{ width: '100%', height: '100%' }} />
-          </Link>
-          <p className="prod-name">{product.name}</p>
-          <p className="prod-price">&#x20b9; {variant.sale_price}</p>
+    if (product) {
+      let tgtUrl = product ? `/product/${product.id}` : '/';
+      let variant = product ? product.variants[0] : {};
+      let imgUrl = variant.images[0].path.split('/').pop();
+      let prodObj = {
+        product: product.id,
+        sku: variant.sku
+      }
+      return (
+        <div className="col-6 col-sm-3 wishlist-prod">
+          <div className="wishlist-prod-container">
+            <div className="float-right"><AddToWishlist wishlistObj={prodObj} /></div>
+            <Link to={tgtUrl}>
+              <img src={images[imgUrl]} alt={product.name} className="img-responsive" style={{ width: '100%', height: '100%' }} />
+            </Link>
+            <p className="prod-name">{product.name}</p>
+            <p className="prod-price">&#x20b9; {variant.sale_price}</p>
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (<div><h1>Your wish list is empty...</h1></div>)
+    }
   }
 }
 export default WishlistProduct
